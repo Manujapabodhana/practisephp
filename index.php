fb219abb4c7d31fff41489072fd2be85396f1b15
Manujapabodhana <rpmanuja123@gmail.com>
2025-06-03 11:22:29 +0530

    second

--- index.php
+++ index.php
@@ -3,26 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo "kkk"; ?></title>
 </head>
 <body>
+    <form method="Get">
+        <input type="text" name="username">
+        <input type="submit" value="Submit">
+    </form>    <?php
+    $username = $_GET['username'];
+    echo $username;
 
-<form method="get">
-    <input typpe="text" name="userchannel">
-    <BUTTON type="submit">Submit</BUTTON>
-</form>
-
-
-<?php 
-
-   $myname= $_GET['userchannel'];
-   echo $myname."is an chanekkk";
-      
+    ?>
 
 
 
-
-?>
 </body>
 </html>
 
