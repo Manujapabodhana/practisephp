7f111d69b493cc98e7c84c1979ae5d73c6756f5a
Manujapabodhana <rpmanuja123@gmail.com>
2025-06-05 20:00:35 +0530

    hh

--- index.php
+++ index.php
@@ -3,18 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo "kkk"; ?></title>
+    <title>hhhh</title>
 </head>
+
 <body>
-    <form method="Get">
-        <input type="text" name="username">
+
+<?php
+
+?>
+
+    <form method="GET">
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+        </div>
+        <br>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+        </div>
+        <br>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <br>
         <input type="submit" value="Submit">
-    </form>    <?php
-    $username = $_GET['username'];
-    echo $username;
+    </form>
 
-    ?>
-
+ 
 
 
 </body>
